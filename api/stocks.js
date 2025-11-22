00aeec4ce50256ea8c38e96b16434998ab08e7f6
CltgAi <avdut90@gmail.com>
2025-11-22 09:51:17 +0100

    Update stocks.js

--- api/stocks.js
+++ api/stocks.js
@@ -12,3 +12,17 @@ module.exports = async (req, res) => {
         res.setHeader("Content-Type", "application/json");
 
         res.status(200).json(response.data.quoteResponse.result[0
+const axios = require("axios");
+
+module.exports = async (req, res) => {
+    try {
+        const symbol = req.query.symbol || "AAPL";
+
+        const url = `https://query1.finance.yahoo.com/v7/finance/quote?symbols=${symbol}`;
+
+        const response = await axios.get(url);
+
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Content-Type", "application/json");
+
+        res.status(200).json(response.data.quoteResponse.result[0
